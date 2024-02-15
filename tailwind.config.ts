343b9e7cf5263cf97d33a9cf9dfabb6a34ec89da
apav-dev <apavlick@yext.com>
2024-02-15 10:03:33 -0700

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
+import type { Config } from "tailwindcss";
+
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
